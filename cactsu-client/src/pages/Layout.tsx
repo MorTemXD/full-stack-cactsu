ffbe587ef6014bec63f77c04f16593b27cbfda01
Macsim <macsim.bilozub@gmail.com>
2024-03-20 18:39:47 +0200

    refactored/delete header

--- cactsu-client/src/pages/Layout.tsx
+++ cactsu-client/src/pages/Layout.tsx
@@ -1,11 +1,9 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
-// import Header from "../components/Header";
 
 const Layout: FC = () => {
     return (
         <div>
-            {/* <Header/> */}
             <div className="container">
                 <Outlet />
             </div>
