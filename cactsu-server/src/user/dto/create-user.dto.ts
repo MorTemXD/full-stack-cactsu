64378e78da8c037138e1667cb17f3bc22f6137d3
dima <dimasilenkoluna@gmail.com>
2024-03-20 19:52:28 +0200

    hotfix/added some regex patterns for validation user

--- cactsu-server/src/user/dto/create-user.dto.ts
+++ cactsu-server/src/user/dto/create-user.dto.ts
@@ -1,19 +1,30 @@
-import { IsString, MinLength } from "class-validator";
+import { IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @IsString()
-    phoneNumber: string;
-
-    @IsString()
-    username: string;
-
-    @IsString()
-    @MinLength(6, { message: "Password is too short" })
-    password: string;
-
-    @IsString()
+    @Matches(/^[a-z ,.'-]+$/i, { message: "Invalid name format" })
+    @IsNotEmpty({message: "Name is required"})
     name: string;
 
     @IsString()
+    @Matches(/^[a-z ,.'-]+$/i, { message: "Invalid surname format" })
+    @IsNotEmpty({message: "Surname is required"})
     surname: string;
+
+    @IsString()
+    @MinLength(8, { message: "Password is too short" })
+    @Matches(/^(?=.*[a-zA-Z])(?=.*\d).{8,}$/, 
+    { message: "Password must contain at least one letter and one digit" })
+    @IsNotEmpty({message: "Password is required"})
+    password: string;
+
+    @IsString()
+    @Matches(/^\+[0-9]{1,3}[0-9]{9}$/, { message: "Phone number must contain 9 digits. And it starts with the country code" })
+    @IsNotEmpty({message: "Phone number is required"})
+    phoneNumber: string;
+
+    @IsString()
+    @Matches(/^[a-zA-Z0-9]+$/, { message: "Invalid username format" })
+    @IsNotEmpty({message: "Username is required"})
+    username: string;
 }
